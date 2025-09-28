498be66e6d38a97f066779fbf0660b77db2bde84
Bertalan Mucsi <berci620@gmail.com>
2025-09-28 15:04:10 +0200

    operators =,+=,+ added

--- HatekonyCPP/Fraction.cpp
+++ HatekonyCPP/Fraction.cpp
@@ -40,10 +40,33 @@ Fraction::Fraction(const Fraction& fraction)
 	_denominator = fraction._denominator;
 }
 
-
 float Fraction::GetFraction()
 {
 	float result = static_cast<float>(_numerator) / _denominator;
+	return result;
+}
+
+Fraction& Fraction::operator=(const Fraction& other) = default;
+
+Fraction& Fraction::operator+=(const Fraction& other)
+{
+	int lcm = LCM(_denominator, other._denominator);
+	int aMult = lcm / _denominator;
+	int bMult = lcm / other._denominator ;
+
+	_numerator *= aMult; _denominator *= aMult;
+
+	_numerator += (other._numerator * bMult);
+
+	this->Simplify();
+
+	return *this;
+}
+
+Fraction Fraction::operator+(const Fraction& other) const 
+{
+	Fraction result(*this);
+	result += other;
 	return result;
 }
 
@@ -70,6 +93,12 @@ int Fraction::GCD(int a, int b)
 		gcd--;
 	}
 	return gcd;
+}
+
+int Fraction::LCM(int a, int b)
+{
+	int result = (a * b) / GCD(a, b);
+	return result;
 }
 
 void Fraction::Simplify()
--- HatekonyCPP/Fraction.h
+++ HatekonyCPP/Fraction.h
@@ -8,10 +8,15 @@ public:
 	Fraction(const Fraction& fraction);
 	float GetFraction();
 
+	Fraction& operator=(const Fraction& other);
+	Fraction& operator+=(const Fraction& other);
+	Fraction operator+(const Fraction& other) const;
+
 private:
 	int _numerator;
 	int _denominator;
 	int GCD(int a, int b);
+	int LCM(int a, int b);
 	void Simplify();
 	void FractionFromFloat(double decimalFraction);
 };
